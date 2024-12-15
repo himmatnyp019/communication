e3e23c8ec2f9c0b8a93821bb1881121f4f725e69
Himmat Neupane <152598388+himmatnyp019@users.noreply.github.com>
2024-12-15 17:22:08 +0900

    Update responsive.js

--- responsive.js
+++ responsive.js
@@ -1,4 +1,3 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
 import { getDatabase, ref,onValue, set, get, child } 
 from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
